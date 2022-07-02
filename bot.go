b27ffdddaad8d75556c706fae45b3a76aafc719a
Ingmund Olsson <ingmund@proton.me>
2022-07-02 21:51:12 +0300

    add: add button

--- bot.go
+++ bot.go
@@ -8,6 +8,14 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+var (
+	wannabeKeyboard = tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Хочу свой VPN", "wannabe"),
+		),
+	)
+)
+
 func createBot(token string, debug bool) (*tgbotapi.BotAPI, error) {
 
 	bot, err := tgbotapi.NewBotAPI(token)
@@ -37,18 +45,40 @@ L1:
 		select {
 		case update := <-updates:
 			if update.Message != nil { // If we got a message
-				if bot.Debug {
-					fmt.Fprintf(os.Stderr, "[%s] %s", update.Message.From.UserName, update.Message.Text)
+				if update.Message.Chat.Type == "private" {
+					if bot.Debug {
+						fmt.Fprintf(os.Stderr, "[%s] %s", update.Message.From.UserName, update.Message.Text)
+					}
+
+					// !!! implement handling return vars
+					//					sendReply(bot, update, update.Message.Text)
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, MsgWelcome)
+					msg.ReplyToMessageID = update.Message.MessageID
+					msg.ReplyMarkup = wannabeKeyboard
+
+					bot.Send(msg)
+
+					break
 				}
 
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-				msg.ReplyToMessageID = update.Message.MessageID
-
-				bot.Send(msg)
+				sendReply(bot, update, WarnGroupsNotAllowed)
 			}
 		case <-stop:
 			fmt.Fprintln(os.Stdout, "[-] Run: Stop signal was received")
+
+			bot.StopReceivingUpdates()
+
 			break L1
 		}
 	}
+}
+
+// sendReply - send text
+func sendReply(bot *tgbotapi.BotAPI, update tgbotapi.Update, text string) (tgbotapi.Message, error) {
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, text)
+	msg.ReplyToMessageID = update.Message.MessageID
+
+	message, err := bot.Send(msg)
+
+	return message, fmt.Errorf("send msg: %w", err)
 }
